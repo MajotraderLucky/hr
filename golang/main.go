440c657446ad55fe943f0fa2cbe20ed88b4be10b
MajotraderLucky <majohead@gmail.com>
2023-11-14 15:01:49 +0300

    fix(main): Внёс изменения в код Рязанов Сергей

--- golang/main.go
+++ golang/main.go
@@ -1,104 +1,131 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"sync"
 	"time"
 )
 
-// ЗАДАНИЕ:
-// * сделать из плохого кода хороший;
-// * важно сохранить логику появления ошибочных тасков;
-// * сделать правильную мультипоточность обработки заданий.
-// Обновленный код отправить через merge-request.
-
-// приложение эмулирует получение и обработку тасков, пытается и получать и обрабатывать в многопоточном режиме
-// В конце должно выводить успешные таски и ошибки выполнены остальных тасков
-
-// A Ttype represents a meaninglessness of our life
+// Ttype - структура задания
 type Ttype struct {
-	id         int
-	cT         string // время создания
-	fT         string // время выполнения
-	taskRESULT []byte
+	id         int64     // изменил тип на int64 для поддержки UnixNano
+	cT         time.Time // время создания задачи
+	fT         time.Time // время завершения задачи
+	taskRESULT []byte    // результат выполнения задачи
+}
+
+// TaskResult - структура результата выполнения задания
+type TaskResult struct {
+	task Ttype // данuые задания
+	err  error // возможная ошибка
 }
 
 func main() {
-	taskCreturer := func(a chan Ttype) {
-		go func() {
-			for {
-				ft := time.Now().Format(time.RFC3339)
-				if time.Now().Nanosecond()%2 > 0 { // вот такое условие появления ошибочных тасков
-					ft = "Some error occured"
+	// Добавил context для корректного завершения всех горутин
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	// Функция создаёт задания и помещает их в канал a
+	taskCreator := func(ctx context.Context, a chan<- Ttype) {
+		for {
+			select {
+			case <-ctx.Done():
+				close(a) // Закрытие канала при завершении контекста
+				return
+			default:
+				isSuccess := time.Now().Nanosecond()%2 == 0 // Условие успешности создания задания
+				now := time.Now()
+				task := Ttype{cT: now, id: now.UnixNano()} // добавил уникальность id
+
+				if !isSuccess {
+					task.cT = time.Time{} // случай ошибочного таска
 				}
-				a <- Ttype{cT: ft, id: int(time.Now().Unix())} // передаем таск на выполнение
+
+				a <- task // Добавляем задания в канал
+			}
+		}
+	}
+
+	// Создаем канал для заданий
+	superChan := make(chan Ttype)
+
+	// Запускаем горутину создания заданий
+	go taskCreator(ctx, superChan)
+
+	workersCount := 5
+	// Канал для сбора выполнененых заданий
+	doneTasks := make(chan TaskResult)
+
+	wg := &sync.WaitGroup{} // sync.WaitGroup для ожидания обработки всех задач
+	wg.Add(workersCount)
+
+	// Запускаем пять рабочих горутин для обработки заданий
+	for i := 0; i < workersCount; i++ {
+		go func() {
+			defer wg.Done() // Уменьшаем счетчик ожидания при завершении горутины
+
+			for t := range superChan { // обработка заданий из канала
+				workerStartTime := time.Now()
+
+				// Проверка времени создания задания и генерация ошибки
+				if t.cT.IsZero() || t.cT.Before(workerStartTime.Add(-20*time.Second)) {
+					doneTasks <- TaskResult{task: t, err: fmt.Errorf("something went wrong")}
+					continue
+				}
+
+				t.fT = time.Now()
+				t.taskRESULT = []byte("task has been successed")
+
+				// Имитация работы над заданием
+				time.Sleep(time.Millisecond * 150)
+				// Отправка исполненного задания в канал
+				doneTasks <- TaskResult{task: t}
 			}
 		}()
 	}
 
-	superChan := make(chan Ttype, 10)
-
-	go taskCreturer(superChan)
-
-	task_worker := func(a Ttype) Ttype {
-		tt, _ := time.Parse(time.RFC3339, a.cT)
-		if tt.After(time.Now().Add(-20 * time.Second)) {
-			a.taskRESULT = []byte("task has been successed")
-		} else {
-			a.taskRESULT = []byte("something went wrong")
-		}
-		a.fT = time.Now().Format(time.RFC3339Nano)
-
-		time.Sleep(time.Millisecond * 150)
-
-		return a
-	}
-
-	doneTasks := make(chan Ttype)
-	undoneTasks := make(chan error)
-
-	tasksorter := func(t Ttype) {
-		if string(t.taskRESULT[14:]) == "successed" {
-			doneTasks <- t
-		} else {
-			undoneTasks <- fmt.Errorf("Task id %d time %s, error %s", t.id, t.cT, t.taskRESULT)
-		}
-	}
-
+	// Горутина ожидает окончания всех рабочих горутин и закрывает канал doneTasks
 	go func() {
-		// получение тасков
-		for t := range superChan {
-			t = task_worker(t)
-			go tasksorter(t)
-		}
-		close(superChan)
+		wg.Wait()
+		close(doneTasks) // Закрываем канал только после завершения всех worker горутин
 	}()
 
-	result := map[int]Ttype{}
-	err := []error{}
-	go func() {
-		for r := range doneTasks {
-			go func() {
-				result[r.id] = r
-			}()
-		}
-		for r := range undoneTasks {
-			go func() {
-				err = append(err, r)
-			}()
-		}
-		close(doneTasks)
-		close(undoneTasks)
-	}()
+	// Обработка полученных заданий и ошибок
+	results, errors := processResults(doneTasks)
+	printResults(results, errors) // Вывод результатов и ошибок
+}
 
-	time.Sleep(time.Second * 3)
+// Функция обрабатывает результаты и возвращает отдельно результаты и ошибки
+func processResults(doneTasks <-chan TaskResult) (map[int64]Ttype, []string) {
+	results := map[int64]Ttype{}
+	errors := []string{}
 
-	println("Errors:")
-	for r := range err {
-		println(r)
+	// Обход по каналу с результатом
+	for taskResult := range doneTasks {
+		if taskResult.err != nil {
+			errors = append(errors, fmt.Sprintf("Task id: %d error: %v", taskResult.task.id, taskResult.err))
+		} else {
+			results[taskResult.task.id] = taskResult.task
+		}
 	}
 
-	println("Done tasks:")
-	for r := range result {
-		println(r)
+	return results, errors
+}
+
+// Функция для вывода результатов и ошибок
+func printResults(results map[int64]Ttype, errors []string) {
+	if len(errors) > 0 {
+		fmt.Println("Errors:")
+		for _, err := range errors {
+			fmt.Println(err)
+		}
+	}
+
+	if len(results) > 0 {
+		fmt.Println("Done tasks:")
+		for _, task := range results {
+			fmt.Printf("ID: %d, cT: %s, fT: %s, result: %s\n", task.id, task.cT, task.fT, string(task.taskRESULT))
+		}
 	}
 }
